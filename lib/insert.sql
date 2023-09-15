1099fe18dd2cb13dad4656e209278b1ec6ac736e
Spencer <eklund.spencer@outlook.com>
2023-09-15 11:26:38 -0600

    added functions to sort columns

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Nice", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "F", "Black", "Rowdy", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 12, "F", "White", "Mean", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Segent Brown", 29, "M", "Brown", "Strict", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 123, "F", "Green", "Nice", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1222, "F", "Black", "Mean", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "F", "White", "Scared", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 13, "M", "Brown", "Nice", 1);
+
+
+
+
+
+
